d80fb7c1cab2b0acfdcb780c98646829c833ba81
Hzye <81663786+Hzye@users.noreply.github.com>
2026-03-20 16:13:35 +1100

    ff and encoder layer

--- transformer.py
+++ transformer.py
@@ -552,3 +552,37 @@ class MultiHeadAttention(nn.Module):
 
         # final linear projection
         return self.output_linear(attention_output)
+    
+
+class FeedForwardSubLayer(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+    
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.ff_sublayer = FeedForwardSubLayer(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, src_mask):
+        attn_output = self.self_attn(x, x, x, src_mask) # q, k, v all x -> self attn, padding mask
+        
+        # dropout + residual + layer norm
+        x = self.norm1(x + self.dropout(attn_output))
+
+        ff_output = self.ff_sublayer(x)
+
+        x = self.norm2(x + self.dropout(ff_output))
+
+        return x
+
